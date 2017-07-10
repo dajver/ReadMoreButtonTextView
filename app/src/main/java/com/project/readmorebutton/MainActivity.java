ea4e3663f8b134b166a62b9bc78dec8bb200e960
gleb <lfqdth5@gmail.com>
2017-07-10 22:38:30 +0300

    added fix for max lines

--- app/src/main/java/com/project/readmorebutton/MainActivity.java
+++ app/src/main/java/com/project/readmorebutton/MainActivity.java
@@ -8,13 +8,15 @@ import com.project.readmorebutton.view.ExpandableTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int MAX_LINES = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ExpandableTextView text = (ExpandableTextView) findViewById(R.id.text);
-        text.setTrimLength(6);
+        text.setTrimLength(MAX_LINES);
         text.setText(getString(R.string.text), TextView.BufferType.NORMAL);
     }
 }
